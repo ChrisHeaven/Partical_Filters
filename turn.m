03ad0dbee5b8a2ff6ded5bde2651876594733621
Yizhang Peng <yp16505@my.bristol.ac.uk>
2017-03-22 17:30:34 +0000

    1.3

---
+++ turn.m
@@ -0,0 +1,29 @@
+function flag = turn(degree,direction) %direction 1:right -1:left
+    %d=4.4 %cm
+    global mL;
+    global mR;
+    
+    if degree > 180;
+        degree = 360 - degree;
+        direction = 1;
+    end
+    wheel=13.6;  %The length of the wheel 
+    length=12.25; %The length between wheels 15.4 12.4 10
+     
+    mL.ActionAtTachoLimit = 'brake';
+    mR.ActionAtTachoLimit = 'brake';
+    
+    n=round(length * pi/wheel*degree);
+   
+    mL.Power = 20*direction;
+    mR.Power = -20*direction;
+    
+    mL.TachoLimit = n;
+    mR.TachoLimit = n;
+    
+    mL.SendToNXT();
+    mR.SendToNXT();
+    
+    mL.WaitFor();
+    mR.WaitFor();
+end
